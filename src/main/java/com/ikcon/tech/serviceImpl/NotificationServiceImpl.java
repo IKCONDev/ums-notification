1841696137ba1c3f7cb05aa7c38d852b1bbc64b9
Bharat <Bharat@ikcontech.com>
2023-11-14 10:52:42 +0530

    updated notification for profile pic

--- src/main/java/com/ikcon/tech/serviceImpl/NotificationServiceImpl.java
+++ src/main/java/com/ikcon/tech/serviceImpl/NotificationServiceImpl.java
@@ -3,16 +3,19 @@ package com.ikcon.tech.serviceImpl;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import com.ikcon.tech.entity.Notification;
 import com.ikcon.tech.repo.NotificationRepo;
 import com.ikcon.tech.service.NotificationService;
-
+import com.ikcon.tech.utils.UserVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,12 +25,23 @@ public class NotificationServiceImpl  implements NotificationService{
 	
 	@Autowired
 	private NotificationRepo notificationRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Override
 	public List<Notification> getAllNotifications(String emailId) {
 		log.info("NotificationController.getAllNotification() entered with args : emailid - "+emailId);
 		List<Notification> notificationList = notificationRepository.getAllNotifications(emailId);
-		return notificationList;
+		List<Notification> updatedList = new ArrayList<>();
+		notificationList.forEach(notification ->{
+			String email = notification.getEmailId();
+			UserVO user =   restTemplate.getForObject("http://UMS-USERS-SERVICE/user/getUser/"+email,UserVO.class);
+			notification.setProfilepic(user.getProfilePic());
+			System.out.println("the notification object is : "+notification);
+			updatedList.add(notification);
+		});
+		return updatedList;
 	}
 
 	@Transactional
